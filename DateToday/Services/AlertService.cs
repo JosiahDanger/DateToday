626e66f532ad5e54dcb0e33a7f3ba6c742d67be6
Joe Shaahin <Josiah.Shaahin@protonmail.com>
2026-08-25 17:42:59 +0100

    Removed AlertViewFactory.

--- DateToday/Services/AlertService.cs
+++ DateToday/Services/AlertService.cs
@@ -1,10 +1,22 @@
 ﻿using Avalonia.Controls;
+using DateToday.Avalonia.ViewModels;
 using DateToday.Avalonia.Views;
 using DateToday.Models;
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
 namespace DateToday.Services;
+
+[SuppressMessage(
+	"Microsoft.Performance",
+	"CA1812:AvoidUninstantiatedInternalClasses",
+	Justification =
+		"""
+			AlertService is registered as a transient service via
+			ServiceCollectionExtensions.AddPresentationServices() and is instantiated by means of
+			dependency injection.
+		""")]
 
 internal sealed class AlertService(Lazy<Window> parentWindow) : IAlertService
 {
@@ -12,8 +24,10 @@ internal sealed class AlertService(Lazy<Window> parentWindow) : IAlertService
 
 	public async Task ShowAlertAsync(AlertFlavour flavour, string message)
 	{
-		AlertView alert = AlertViewFactory.CreateAlertView(flavour, message);
+		AlertModel alertModel = new(flavour, message);
+		AlertViewModel alertViewModel = new(alertModel);
+		AlertView alertView = new() { DataContext = alertViewModel };
 
-		await alert.ShowDialog(_parentWindow.Value).ConfigureAwait(false);
+		await alertView.ShowDialog(_parentWindow.Value).ConfigureAwait(false);
 	}
 }
